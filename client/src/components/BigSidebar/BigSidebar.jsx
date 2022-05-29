9dd059c53c17411997a785539914556a9d19cf8f
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-05-29 08:02:35 +0200

    added sidebar with navlinks

--- client/src/components/BigSidebar/BigSidebar.jsx
+++ client/src/components/BigSidebar/BigSidebar.jsx
@@ -1,9 +1,24 @@
+import { useAppContext } from "../../context/appContext";
+import NavLinks from "../NavLinks";
+import Logo from "../Logo/Logo";
 import Wrapper from "./BigSidebar.styles";
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <div>BigSidebar</div>
+      <div
+        className={
+          showSidebar ? "sidebar-container " : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
--- client/src/components/SmallSidebar/SmallSidebar.jsx
+++ client/src/components/SmallSidebar/SmallSidebar.jsx
@@ -1,9 +1,30 @@
 import Wrapper from "./SmallSidebar.styles";
+import Logo from "../Logo/Logo";
+import { useAppContext } from "../../context/appContext";
+import { FaTimes } from "react-icons/fa";
+import NavLinks from "../NavLinks";
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
+
   return (
     <Wrapper>
-      <div>small sidebar</div>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
