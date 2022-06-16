7d770fb0f906f900c27410a83af8489fea9afe1a
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-06-16 08:23:31 +0200

    added sequrity packages for Express-server + established static assets

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,19 @@
 import { register, login, updateUser } from "../controllers/authController.js";
 import express from "express";
 const router = express.Router();
+import rateLimiter from "express-rate-limit";
+
 import authenticateUser from "../middlewares/auth.js";
 
-router.route("/register").post(register);
-router.route("/login").post(login);
+// rate-limiting middleware for Express
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+router.route("/register").post(apiLimiter, register);
+router.route("/login").post(apiLimiter, login);
 router.route("/updateUser").patch(authenticateUser, updateUser);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -19,15 +19,42 @@ import notFoundMiddleware from "./middlewares/not-found.js";
 import errorHandlerMiddleware from "./middlewares/error-handler.js";
 import authenticateUser from "./middlewares/auth.js";
 
+// ------------Security Packages------------ //
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+
+// ------------Production------------ //
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
+// -------------------------------------------------------------- //
+
+const __dirname = dirname(fileURLToPath(import.meta.url)); // because we're using ES6-modules not common.js
+
+app.use(express.static(path.resolve(__dirname, "../client/build"))); // static assets
+
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
+
 app.use(express.json()); // make json-data available
+app.use(helmet()); // secure Express-app by setting various HTTP headers
+app.use(xss()); // node-Connect-middleware to sanitize user input coming from POST body, GET queries, and url params
+app.use(mongoSanitize()); // Sanitizes user-supplied data to prevent MongoDB Operator Injection
+
 app.get("/api/v1", (req, res) => {
   res.json("Welcome!");
 });
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+// direct to index.html for react-router after the 2 routes above
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
